321017b2914df89397d5b1b6a56381436008f72e
Shubham Maheshwari <shubhammaheshwari@Shubhams-MacBook-Air.local>
2025-11-25 00:12:53 +0530

    initial commit

---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import { View } from "react-native";
+import Login from "./login/_layout";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+      }}
+    >
+      <Login />
+    </View>
+  );
+}
---
+++ app/login/_layout.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const _layout = () => {
+  return (
+    <View>
+      <Text>PulseMeet</Text>
+      <Text>Where interests meet.</Text>
+    </View>
+  );
+};
+
+export default _layout;

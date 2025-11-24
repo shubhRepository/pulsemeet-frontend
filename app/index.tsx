a82062016a9f99dfc236cbad31f91362c9a4d2e9
Shubham Maheshwari <shubhammaheshwari@Shubhams-MacBook-Air.local>
2025-11-25 00:22:17 +0530

    setup nextwind styling framework

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,10 @@
 import { View } from "react-native";
+import "./global.css";
 import Login from "./login/_layout";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-      }}
-    >
+    <View>
       <Login />
     </View>
   );
